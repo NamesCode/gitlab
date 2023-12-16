6bd15a59510433289233605fb715ad9b02a6590e
Name <namesexistsinemails@gmail.com>
2023-12-16 23:06:40 +0000

    Fix: make `patch.sh` install submodule

--- patch.sh
+++ patch.sh
@@ -5,6 +5,8 @@ case $dir in
 *) git clone https://github/catppuccin/gitlab && cd gitlab ;;
 esac
 
+git submodule update --init --recursive
+
 cp ./color.rb ./gitlab/lib/gitlab/color.rb
 
 read -e -p "Which flavor would you like to use? mocha/macchiato/frappe/latte: " -i "mocha" chosen_flavor
